9551e6fcc78fd63b3926c9496ee39cc38c2525ed
Mugil99 <124775550+Mugil99@users.noreply.github.com>
2023-04-17 12:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 //complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	get getName(){
-		return this.name;
-	}
-	set setAge(age){
-		this.age=age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set age(age) {
+    this._age = age;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
+
+// Example usage
+const person = new Person("John", 25);
+console.log(person.name); // "John"
+person.age = 30;
+
+const student = new Student("Jane", 20);
+console.log(student.name); // "Jane"
+student.study(); // "Jane is studying"
+
+const teacher = new Teacher("Bob", 35);
+console.log(teacher.name); // "Bob"
+teacher.teach(); // "Bob is teaching"
+
 
 // Do not change the code below this line
 window.Person = Person;
